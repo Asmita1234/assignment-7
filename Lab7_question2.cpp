056d43b9c0a58295a0578a025b33269b520e985b
Asmita1234 <31311738+Asmita1234@users.noreply.github.com>
2017-11-12 18:35:21 +0530

    Update Lab7_question2.cpp

--- Lab7_question2.cpp
+++ Lab7_question2.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 class Box {
    public:
-      double length;         // Length of a box
-      double breadth;        // Breadth of a box
-      double height;         // Height of a box
+      double length;         
+      double breadth;        
+      double height;         
 
-      // Member functions declaration
+      
       double getVolume(void);
       void setLength( double len );
       void setBreadth( double bre );
       void setHeight( double hei );
 };
 
-// Member functions definitions
+
 double Box::getVolume(void) {
    return length * breadth * height;
 }
@@ -30,27 +30,26 @@ void Box::setHeight( double hei ) {
    height = hei;
 }
 
-// Main function for the program
+
 int main() {
-   Box Box1;                // Declare Box1 of type Box
-   Box Box2;                // Declare Box2 of type Box
-   double volume = 0.0;     // Store the volume of a box here
+   Box Box1;                
+   Box Box2;                
+   double volume = 0.0;     
  
-   // box 1 specification
+  
    Box1.setLength(6.0); 
    Box1.setBreadth(7.0); 
    Box1.setHeight(5.0);
 
-   // box 2 specification
+  
    Box2.setLength(12.0); 
    Box2.setBreadth(13.0); 
    Box2.setHeight(10.0);
 
-   // volume of box 1
    volume = Box1.getVolume();
    cout << "Volume of Box1 : " << volume <<endl;
 
-   // volume of box 2
+   
    volume = Box2.getVolume();
    cout << "Volume of Box2 : " << volume <<endl;
    return 0;
